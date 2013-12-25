55474b56b90497b7158d92ac36c7f1bb8677c9a5
shunirr <m@s5r.jp>
2013-12-25 14:50:24 +0900

    1 日でキャッシュを捨てるようにした

--- lib/sns-icon-redirector/application.rb
+++ lib/sns-icon-redirector/application.rb
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+require 'http'
+
 module SnsIconRedirector
   class Application < Sinatra::Base
     def initialize
@@ -9,24 +11,36 @@ module SnsIconRedirector
       @icons = {}
     end
 
+    def get_icon(user)
+      icon = {}
+      icon[:expire_at] = Time.now + 24 * 60 * 60
+      case user
+      when /^[a-zA-Z0-9_]+$/
+        icon[:url] = @twitter_icon.get params[:user]
+      when /\(fb:(\d+)\)$/
+        icon[:url] = @facebook_icon.get $1
+      end
+      if icon[:url]
+        icon
+      else
+        nil
+      end
+    end
+
     get '/:user' do
       user = params[:user]
-      url = @icons[user]
-      if url.nil? then
-        case user
-        when /^[a-zA-Z0-9_]+$/
-          url = @twitter_icon.get params[:user]
-        when /\(fb:(\d+)\)$/
-          url = @facebook_icon.get $1
+      icon = @icons[user] || get_icon(user)
+      if icon
+        if icon[:expire_at] > Time.now
+          if HTTP.head(icon[:url]).status.to_s =~ /^40\d$/
+            icon = get_icon user
+          end
         end
-        @icons[params[:user]] = url if url
-      end
-    
-      if url.nil? then
+        @icons[user] = icon
+        redirect icon[:url]
+      else
         status 404
         ''
-      else
-        redirect url
       end
     end
   end
