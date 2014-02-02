5030031321b44e628d4cd29cbecb27909aa1803a
shunirr <m@s5r.jp>
2014-02-02 12:49:41 +0900

    pit やめる

--- lib/sns-icon-redirector/twitter_icon.rb
+++ lib/sns-icon-redirector/twitter_icon.rb
@@ -1,23 +1,15 @@
 # -*- encoding: utf-8 -*-
 
-require 'pit'
 require 'twitter'
 
 module SnsIconRedirector
   class TwitterIcon < Icon
     def initialize
-      pit = Pit.get("twitter", :require => {
-        'consumer_key'        => "YOUR_CONSUMER_KEY",
-        'consumer_secret'     => "YOUR_CONSUMER_SECRET",
-        'access_token'        => "YOUR_ACCESS_TOKEN",
-        'access_token_secret' => "YOUR_ACCESS_SECRET"
-      })
-      
       @client = Twitter::REST::Client.new do |config|
-        config.consumer_key       = pit['consumer_key']
-        config.consumer_secret    = pit['consumer_secret']
-        config.oauth_token        = pit['access_token']
-        config.oauth_token_secret = pit['access_token_secret']
+        config.consumer_key       = ENV['consumer_key']
+        config.consumer_secret    = ENV['consumer_secret']
+        config.oauth_token        = ENV['access_token']
+        config.oauth_token_secret = ENV['access_token_secret']
       end
     end
 
