52e9e43d1992fc5dc7a1900676d2c1face2295fe
shunirr <m@s5r.jp>
2014-02-05 17:34:28 +0900

    oauth -> access

--- lib/sns-icon-redirector/twitter_icon.rb
+++ lib/sns-icon-redirector/twitter_icon.rb
@@ -6,10 +6,10 @@ module SnsIconRedirector
   class TwitterIcon < Icon
     def initialize
       @client = Twitter::REST::Client.new do |config|
-        config.consumer_key       = ENV['consumer_key']
-        config.consumer_secret    = ENV['consumer_secret']
-        config.oauth_token        = ENV['access_token']
-        config.oauth_token_secret = ENV['access_token_secret']
+        config.consumer_key        = ENV['consumer_key']
+        config.consumer_secret     = ENV['consumer_secret']
+        config.access_token        = ENV['access_token']
+        config.access_token_secret = ENV['access_token_secret']
       end
     end
 
